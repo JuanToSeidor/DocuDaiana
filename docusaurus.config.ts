35602afeb22883ad009082ffde19573c5066e8b3
JuanToSeidor <jdtorres@seidoranalytics.com>
2025-04-16 15:04:55 -0300

    Config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://daianadmo.seidoranalytics.com/',
+  url: 'https://docudaina.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
